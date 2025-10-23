143bdf3d2661e7b09a96f81e4654cd2e7c2c9194
mohitkushwah14032000 <mk7514004@gmail.com>
2025-10-24 01:32:32 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className='min-h-screen flex items-center justify-center text-[2rem]'>
       <h1>MOHIT</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, harum!</p>
     </div>
   )
 }
