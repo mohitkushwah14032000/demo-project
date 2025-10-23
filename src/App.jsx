a37031052151695a042445f41ef61ec559d1c623
mohitkushwah14032000 <mk7514004@gmail.com>
2025-10-24 01:12:55 +0530

    demo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center text-[2rem]'>
+      <h1>MOHIT</h1>
+    </div>
+  )
+}
+
+export default App
